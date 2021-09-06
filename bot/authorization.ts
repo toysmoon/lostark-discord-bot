f31e325d1afbf473c4e2381e425e6e306d87a4c1
toysmoon <toysmoon@gmail.com>
2021-09-06 14:57:19 +0900

    fix: update buffer

--- bot/authorization.ts
+++ bot/authorization.ts
@@ -8,9 +8,9 @@ export default function authorization(
   const PUBLIC_KEY =
     '829340a3245eeca9e59fb128e0756870df4c527c3662984da90b2e5e20ce9f45';
 
-  const signature = req.headers['X-Signature-Ed25519'] as string;
-  const timestamp = req.headers['X-Signature-Timestamp'] as string;
-  const body = req.body;
+  const signature = (req.headers['X-Signature-Ed25519'] as string) ?? '';
+  const timestamp = (req.headers['X-Signature-Timestamp'] as string) ?? '';
+  const body = req.body ?? '';
 
   const isVerified = nacl.sign.detached.verify(
     Buffer.from(timestamp + body),

1330069505397ac55db9b9b786d73d40e5ada2a8
toysmoon <toysmoon@gmail.com>
2021-09-06 22:53:38 +0900

    fix: type error

--- bot/discord/command/add.ts
+++ bot/discord/command/add.ts
@@ -4,7 +4,18 @@ import addCharacter from '../../database/addCharacter';
 export default async function add(id: string, msg: Message) {
   const [, sLevel, name] = msg.content.split(' ');
   const level = Number(sLevel);
+  const date = new Date(0);
 
-  addCharacter({ id, name, level });
+  addCharacter({
+    id,
+    name,
+    level,
+    chaos: date,
+    gardian: date,
+    abyss: date,
+    argos: date,
+    commander: date,
+  });
+
   msg.reply(`${level} ${name} 추가되었습니다.`);
 }

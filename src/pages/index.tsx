5576f2228b7f1050440b60dce77d5fdde36a28e2
toysmoon <toysmoon@gmail.com>
2021-09-06 12:14:40 +0900

    fix: message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 import React from 'react';
 
 const Main: NextPage = () => {
-  return <div>It's lostark discord bot</div>;
+  return <div>It's lostark discord bot TT</div>;
 };
 
 export default Main;
